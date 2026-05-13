8532975ee7230a93932c36295222c01939c2cd74
FAE047 <faithengabou@hotmail.com>
2026-05-13 11:45:34 +0200

    Koblet Sanity og REACT (CORS origins), og lagt til API Tokens)

--- react/src/helpers/sanityClient.js
+++ react/src/helpers/sanityClient.js
@@ -1,11 +1,11 @@
 import { createClient } from '@sanity/client'
 
 export const sanityClient = createClient({
-    projectId: 'YOUR_PROJECT_ID',
+    projectId: '18dbr3jz',
     dataset: 'production',
     useCdn: true,
     apiVersion: '2026-04-22',
-    token: 'YOUR_API_TOKEN'
+    token: 'skbHDj4dByjSWB5MG1MiH7Ox6xyRQuKXuJPLrvLrGcdfSxLTks8J1BrutXkhLRLYeumfH4nKD2nZMcp95j9nQ8vKqSeZa7FkgawkaoJDyFeMRhetqxDLVFnprvyjIKRrymoxrrTvKgpYPuBbPivH1CkBl6DwGHARIQSrabo5F0mp4exaMnlT'
 })
 
 export default sanityClient
